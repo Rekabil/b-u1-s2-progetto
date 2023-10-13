ca265629575e4d92849197f4cbb48769cf95a4b8
Bilgen Kaan Remzi <137759282+Rekabil@users.noreply.github.com>
2023-10-13 10:13:52 +0200

    first Libri

---
+++ Main.java
@@ -0,0 +1,9 @@
+import entites.Libri;
+
+public class Main {
+    public static void main(String[] args) {
+        Libri book1 = new Libri("Lord of the Rings", 1952, 599, "J. R. R. Tolkien", "Fantasy");
+
+
+    }
+}
---
+++ entites/Libri.java
@@ -0,0 +1,42 @@
+package entites;
+
+import java.util.Random;
+
+public class Libri {
+    Long id;
+    String titolo;
+    int annoPublicazione;
+    int numeroPagine;
+    String autore;
+    String genere;
+
+    public Libri(String titolo,int annoPublicazione, int numeroPagine, String autore, String genere) {
+        Random rndm = new Random();
+        this.id = rndm.nextLong();
+        this.titolo = titolo;
+        this.annoPublicazione = annoPublicazione;
+        this.numeroPagine = numeroPagine;
+        this.autore = autore;
+        this.genere = genere;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getAnnoPublicazione() {
+        return annoPublicazione;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+}
